5cdcca38374ea6f814248e2018711ed10ee291e7
Zavi <zavlagas@hotmail.com>
2021-01-01 12:02:55 +0200

    Create-React-App (Developing Folder In Main)

--- src/main/java/fashion/controllers/SignUpController.java
+++ src/main/java/fashion/controllers/SignUpController.java
@@ -11,13 +11,13 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class SignUpController {
 
