8f90e8b7f8dc40d4667193144e746331a7bf4eba
Zavi <zavlagas@hotmail.com>
2021-01-15 13:34:22 +0200

    PopUpContainer brandList For Edit

--- src/main/frondend/developing-frondend/src/components/BrandsInspector.jsx
+++ src/main/frondend/developing-frondend/src/components/BrandsInspector.jsx
@@ -7,26 +7,37 @@ class BrandsInspector extends Component {
     this.state = {
       brandsList: [],
       brandIsDeleted: false,
+      editBrand: {},
+      popUpForEdit: false,
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
     const buttonAction = event.target.textContent;
+    const targetedBrand = event.target.value;
     if (buttonAction === "delete") {
-      const brandIdForDelete = event.target.value;
-      const endPoint = `http://localhost:8080/FashionProject/api/brands/${brandIdForDelete}`;
+      const endPoint = `http://localhost:8080/FashionProject/api/brands/${targetedBrand}`;
       axios.delete(endPoint).then((response) => {
         console.log(response);
         this.setState({
-          handleDelete: response.data,
+          brandIsDeleted: response.data,
         });
       });
-      const tempBrandList = this.state.brandsList.filter(
-        (brand) => brand.id !== brandIdForDelete
-      );
+      const tempBrandList = this.state.brandsList.filter((brand) => {
+        return brand.id != targetedBrand;
+      });
       this.setState({
         brandsList: tempBrandList,
+      });
+    }
+    if (buttonAction === "Edit") {
+      const endPoint = `http://localhost:8080/FashionProject/api/brands/${targetedBrand}`;
+      axios.get(endPoint).then((response) => {
+        this.setState({
+          editBrand: response.data,
+          popUpForEdit: true,
+        });
       });
     }
   }
@@ -41,42 +52,60 @@ class BrandsInspector extends Component {
     });
   }
 
+  popUpContainer() {
+    return (
+      <>
+        <div className="popup-content">
+          <h1>Hi from Popup</h1>
+        </div>
+      </>
+    );
+  }
+
   render() {
     return (
       <>
         <div id="brands-container">
-          <h2 className="brands-title-list">Brands List</h2>
-          <table className="brands-table" onClick={this.handleClick}>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Logo</th>
-                <th>Name</th>
-                <th>Edit</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.brandsList.map((brand) => {
-                return (
+          {this.state.popUpForEdit ? (
+            this.popUpContainer()
+          ) : (
+            <>
+              <h2 className="brands-title-list">Brands List</h2>
+              <table className="brands-table" onClick={this.handleClick}>
+                <thead>
                   <tr>
-                    <td>{brand.id}</td>
-                    <td>
-                      <img src={brand.imagePath} />
-                    </td>
-                    <td>{brand.name}</td>
-                    <td>
-                      <button value={brand.id}>Edit</button>
-                    </td>
-                    <td>
-                      <button value={brand.id}>delete</button>
-                    </td>
+                    <th>Id</th>
+                    <th>Logo</th>
+                    <th>Name</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                   </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          <p>{this.state.brandIsDeleted ? "Succesfully Deleted" : ""}</p>
+                </thead>
+                <tbody>
+                  {this.state.brandsList.map((brand) => {
+                    return (
+                      <tr>
+                        <td>{brand.id}</td>
+                        <td>
+                          <img src={brand.imagePath} />
+                        </td>
+                        <td>{brand.name}</td>
+                        <td>
+                          <button value={brand.id}>Edit</button>
+                        </td>
+                        <td>
+                          <button value={brand.id}>delete</button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              <p className="message">
+                {this.state.brandIsDeleted ? "Succesfully Deleted" : ""}
+              </p>
+            </>
+          )}
         </div>
       </>
     );
