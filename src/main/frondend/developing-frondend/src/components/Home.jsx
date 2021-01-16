52e0d269562ef2485f67971a01858275623e65e6
Zavi <zavlagas@hotmail.com>
2021-01-16 19:17:19 +0200

    Changes To MySql Db

--- src/main/frondend/developing-frondend/src/components/Home.jsx
+++ src/main/frondend/developing-frondend/src/components/Home.jsx
@@ -9,8 +9,28 @@ class Home extends Component {
   render() {
     return (
       <>
-        <main>
-          <h1>WELCOME TO DASHBOARD</h1>
+        <main className="home-container">
+          <div className="posts-list-container">
+            <article className="post-container">
+              <section className="post-header-user-container">
+                <img
+                  className="user-image"
+                  src="https://picsum.photos/100/100"
+                />
+                <p className="user-dtls">Nick Zavlagas</p>
+              </section>
+              <section className="post-image-container">
+                <img
+                  className="post-image"
+                  src="https://picsum.photos/800/800"
+                />
+              </section>
+              <section className="post-interact-container">
+                <i class="far fa-heart"></i>
+              </section>
+            </article>
+          </div>
+          <aside>Aside</aside>
         </main>
       </>
     );
